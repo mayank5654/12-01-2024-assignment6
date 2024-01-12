c181013f7c146eb61158ff68f869c2ee038cedbd
mayank5654 <mayankkaushal99@gmail.com>
2024-01-12 15:24:21 +0530

    intial commit

---
+++ assignment6/Controllers/MoviesController.cs
@@ -0,0 +1,69 @@
+﻿using assignment6.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace assignment6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+
+        private static List<MovieModel> movies = new List<MovieModel>();
+
+        // GET: api/movies
+        [HttpGet]
+        public ActionResult<IEnumerable<MovieModel>> Get()
+        {
+            return Ok(movies);
+        }
+
+        // GET: api/movies/1
+        [HttpGet("{id}")]
+        public ActionResult<MovieModel> Get(int id)
+        {
+            var movie = movies.Find(m => m.Id == id);
+            if (movie == null)
+                return NotFound();
+
+            return Ok(movie);
+        }
+
+        // POST: api/movies
+        [HttpPost]
+        public ActionResult<MovieModel> Post([FromBody] MovieModel newMovie)
+        {
+            newMovie.Id = movies.Count + 1;
+            movies.Add(newMovie);
+            return CreatedAtAction(nameof(Get), new { id = newMovie.Id }, newMovie);
+        }
+
+        // PUT: api/movies/1
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] MovieModel updatedMovie)
+        {
+            var existingMovie = movies.Find(m => m.Id == id);
+            if (existingMovie == null)
+                return NotFound();
+
+            existingMovie.Title = updatedMovie.Title;
+            existingMovie.Year = updatedMovie.Year;
+            // Update other properties as needed
+
+            return NoContent();
+        }
+
+        // DELETE: api/movies/1
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var movieToRemove = movies.Find(m => m.Id == id);
+            if (movieToRemove == null)
+                return NotFound();
+
+            movies.Remove(movieToRemove);
+            return NoContent();
+        }
+    }
+}
+
---
+++ assignment6/Models/MovieModel.cs
@@ -0,0 +1,9 @@
+﻿namespace assignment6.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+    }
+}
